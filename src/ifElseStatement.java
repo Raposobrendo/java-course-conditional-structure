e31e46d9d4f3f72df89b34d1bf602122c8f3ee4d
RaposoBrendo <raposobrendo@gmail.com>
2025-10-18 12:04:08 -0300

    First commit. If/else statement

---
+++ src/ifElseStatement.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ifElseStatement {
+    public static void main (String[] Args){
+        Scanner sc = new Scanner(System.in);
+
+        double income, tax;
+
+        System.out.print("Enter your income for us to calculate your income tax: ");
+        income = sc.nextDouble();
+
+        if(income <= 2000){
+            System.out.println("No income tax!");
+        }
+        else if (income > 2000 && income <= 3000){
+            tax = (income - 2000) * 0.08;
+            System.out.printf("Income tax: R$%.2f", tax);
+        }
+        else if (income > 3000 && income <= 4500){
+            tax = 80 + ((income - 3000) * 0.18);
+            System.out.printf("Income tax: R$%.2f", tax);
+        }
+        else{
+            tax = 350 + ((income - 4500) * 0.28);
+            System.out.printf("Income tax: R$%.2f", tax);
+        }
+    }
+}
